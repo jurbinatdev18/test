cf4b35b18abd0b4efd242ddf1fbe0f9acb479c26
Jhonatan Urbina <jhonatanurbinat@gmail.com>
2026-03-06 04:15:27 -0500

    commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// crea un servidor express basico      
+// carge las variables desde el archivo .env
+
+const unusedVar = 42;
+
+const express = require('express');
+// cargue las variables desde el archivo .env
+require('dotenv').config();
+const app = express();
+// agrega logica a esta variable port para que use la variable de entorno
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Mibanco');
+});
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+  });
+}
+
+module.exports = app;
