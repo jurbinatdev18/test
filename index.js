b39e88faaf76b071dbeb393f1c594354cca6370a
unknown <jurbinat@student.universidadviu.com>
2026-07-07 17:43:50 -0500

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello Mibancov3');
+  res.send('Hello App');
 });
 
 if (require.main === module) {
